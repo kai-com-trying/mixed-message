dcd1a7d1c6aa3025263b2cbe90ec0aaf7c2de934
kaicomtrying <172279975+kai-com-trying@users.noreply.github.com>
2024-11-03 20:13:48 +0800

    created array for messages

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log('hello')
+const messages = ['Alex Hormozi: "Your business should solve a real, urgent problem—focus on that, and success will follow."',
+                  'MJ DeMarco: "True wealth comes from creating assets that work for you, not from trading hours for dollars."',
+                  'Steve Jobs: "Don’t just make products; make experiences that inspire and transform lives."',
+                  'Noah Kagan: "Test ideas quickly—launch something small today and learn from real feedback."',
+                  'Steven Bartlett: "Your brand story matters; authenticity builds connection and drives growth."',
+                  'Ali Abdaal: "Optimize your day-to-day routines, and you\'ll slowly build a life that feels aligned and fulfilling."']
+
